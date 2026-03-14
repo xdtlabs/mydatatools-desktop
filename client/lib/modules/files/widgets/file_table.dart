0f59febed54dafd6f485fc87782ab4e49563459e
Mike Nimer <mnimer@gmail.com>
2026-03-14 16:33:47 -0500

    style: improve readability of byte size formatting in file table widget

--- client/lib/modules/files/widgets/file_table.dart
+++ client/lib/modules/files/widgets/file_table.dart
@@ -350,6 +350,10 @@ class _FileTable extends State<FileTable> {
     if (bytes <= 0) return "0 B";
     const suffixes = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
     var i = (log(bytes) / log(1024)).floor();
-    return ((bytes / pow(1024, i)).toStringAsFixed(1)).replaceAll(RegExp(r'\.0$'), '') + ' ' + suffixes[i];
+    return ((bytes / pow(1024, i)).toStringAsFixed(
+          1,
+        )).replaceAll(RegExp(r'\.0$'), '') +
+        ' ' +
+        suffixes[i];
   }
 }
